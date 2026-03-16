01347975fe308d0c1e89e271fa3d76f9e6a98cb6
Mindoed-PC <viktor.holanec@gmail.com>
2026-03-16 14:38:13 +0100

    feat: added a compile and run script

---
+++ ija2025-ukol1/run.sh
@@ -0,0 +1,11 @@
+JUNIT="../junit-platform-console-standalone-1.11.4.jar"
+
+# Kompilace s vyuzitim JUnit:
+echo ""
+echo "Compiling tests..."
+javac -cp .:${JUNIT} -d cls ija/homework1/scheme/Homework1Test.java
+
+# Spusteni testu s vyuzitim JUnit:
+echo ""
+echo "Running tests..."
+java -ea -jar ./${JUNIT} execute --cp cls --scan-class-path
